b3360f7f87d1aff488d8046f71d99659c55c6a73
TYJUN1201 <105921933+TYJUN1201@users.noreply.github.com>
2023-11-07 13:22:43 +0800

    .

--- Radius_Angle_Algorithm_2.0.py
+++ Radius_Angle_Algorithm_2.0.py
@@ -31,23 +31,23 @@ def up_sample(waypoints, factor):
     n = len(p)
     return [[i / factor * p[(j+1) % n][0] + (1 - i / factor) * p[j][0],i / factor * p[(j+1) % n][1] + (1 - i / factor) * p[j][1]] for j in range(n) for i in range(factor)]
 
-def get_target_point(params):
-    waypoints = up_sample(get_waypoints_ordered_in_driving_direction(params), 15)
+
+def get_target_point(params, waypoints_list, radius): # radius = 1.0
     car = [params['x'], params['y']]
-    distances = [dist(p, car) for p in waypoints]
+    distances = [dist(p, car) for p in waypoints_list]
     min_dist = min(distances)
     i_closest = distances.index(min_dist)
-    n = len(waypoints)
-    waypoints_starting_with_closest = [waypoints[(i+i_closest) % n] for i in range(n)]
-    r = params['track_width'] * 1.0
+    n = len(waypoints_list)
+    waypoints_starting_with_closest = [waypoints_list[(i+i_closest) % n] for i in range(n)]
+    r = params['track_width'] * radius
     is_inside = [dist(p, car) < r for p in waypoints_starting_with_closest]
     i_first_outside = is_inside.index(False)
     return waypoints_starting_with_closest[i_first_outside]
 
-def get_target_steering_degree(params):
-    tx, ty = get_target_point(params)
-    car_x = params['x']
+def get_target_steering_degree(params, p_x_y):
+    tx, ty = p_x_y
     car_y = params['y']
+    car_x = params['x']
     dx = tx-car_x
     dy = ty-car_y
     heading = params['heading']
@@ -55,12 +55,27 @@ def get_target_steering_degree(params):
     steering_angle = target_angle - heading
     return angle_mod_360(steering_angle)
 
+def get_max_ideal_steering_degree(params):
+    waypoints = up_sample(get_waypoints_ordered_in_driving_direction(params), 10)
+    steering_degree_list = []
+    steering_degree_list.append(get_target_steering_degree(params, get_target_point(params, waypoints, 0.8)))
+    steering_degree_list.append(get_target_steering_degree(params, get_target_point(params, waypoints, 1.0)))
+    steering_degree_list.append(get_target_steering_degree(params, get_target_point(params, waypoints, 1.2)))
+
+    angle_threshold = 14
+    steering_degree_under_threshold_list = [steering_angle for steering_angle in steering_degree_list if steering_angle <= angle_threshold]
+    
+    if steering_degree_under_threshold_list:
+        return max(steering_degree_under_threshold_list)
+    else:
+        return angle_threshold
+    
 def score_steer_to_point_ahead(params):
-    best_stearing_angle = get_target_steering_degree(params)
+    ideal_stearing_angle = get_max_ideal_steering_degree(params)
     steering_angle = params['steering_angle']
-    error = (steering_angle - best_stearing_angle) / 60.0
+    error = (steering_angle - ideal_stearing_angle) / 60.0
     score = 1.0 - abs(error)
-    return max(score, 0.01)
+    return max(score, 0.01)    
 
 def reward_function(params):
     return float(score_steer_to_point_ahead(params))
